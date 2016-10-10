8b8ba0ad8b6b5ab90e4fd4c5977725a3570d8b9b
lukas luehr <lukas.luehr@haw-hamburg.de>
2016-10-10 13:39:37 +0200

    addet documentation for the Arduino Programm

--- arduino/programm/programm/programm.ino
+++ arduino/programm/programm/programm.ino
@@ -6,6 +6,13 @@ int addPin0   = 8;
 int addPin1   = 9;
 int addPin2   = 10;
 
+/* uint8_t readByte(int,bool*)
+ * parameters:
+ *  signalPin : The Pin for the ZACWire Data Line
+ *  error     : A Pointer to a variable, wich will be set to true in case of an error.
+ * returns:
+ *  8 bit value read ofer the ZACWire protocoll
+ */
 uint8_t readByte(int signalPin,bool *error){
     uint16_t byte = 0;
     bool correkt = true;
@@ -34,6 +41,15 @@ uint8_t readByte(int signalPin,bool *error){
     return (uint8_t)(byte >> 1); // return just the date byte
 }
 
+/*uint8_t readWord(int,bool*)
+ * intern wird das ReadByte verwendet.
+ * parameters:
+ *  signalPin : The Pin for the ZACWire Data Line
+ *  error     : A Pointer to a variable, wich will be set to true in case of an error.
+ * returns:
+ *  8 bit value read ofer the ZACWire protocoll 
+ */
+ 
 uint16_t readWord(int signalPin,bool *error){
   uint8_t byte1 = readByte(signalPin,error);
   uint8_t byte2 = readByte(signalPin,error);
@@ -41,7 +57,17 @@ uint16_t readWord(int signalPin,bool *error){
 }
 
 
+/* float readTemperature(int,signalPin)
+ * Reads the Themperature form the Sensor and Convertes it to degrees Celsius
+ * Parameter:
+ *  powerPin  : The Pin, wich is used to Provide the Sensor with Power
+ *  signalPin : The Pin for the ZACWire Data Line
+ * returns :
+ *  NAN if there was na error
+ *  the Temperatur in degree Celsius.
+ */
 float readTemperature(int powerPin,int signalPin){
+  
   bool error=false;
   digitalWrite(powerPin, HIGH);  //Aktivate sensor 
   delayMicroseconds(60);  // etwas zeit für den sensor
@@ -52,16 +78,19 @@ float readTemperature(int powerPin,int signalPin){
 }
 
 
+/*void setup(void)
+ * Will be Called once of the Micro Controller Powers Up
+ */
 void setup() {
-    Serial.begin(9600); // set up the serial port
+    Serial.begin(9600); // init serail Connection
 
     
-    pinMode(addPin0, OUTPUT);
+    pinMode(addPin0, OUTPUT); // Sets the Pins used for the Multiplexer address line to Output
     pinMode(addPin1, OUTPUT);
     pinMode(addPin2, OUTPUT);
   
     
-    digitalWrite(addPin0, LOW);
+    digitalWrite(addPin0, LOW); //And Puts them in a defined state
     digitalWrite(addPin1, LOW);
     digitalWrite(addPin2, LOW);
 
@@ -70,16 +99,30 @@ void setup() {
     pinMode(12,INPUT);
 }
 
+/* bool needToSend(void)
+ * returns : 
+ *  Checks if there is a data waiting in the serial buffer, if so, the Functions removes all waiting date and returns true
+ *  if there was no data waiting, it returnes false.
+ * 
+ *  
+ */
 bool needToSend(){
   bool toret =  Serial.available();
-  if(toret)while(Serial.available())Serial.read();
+  if(toret)
+    while(Serial.available())
+      Serial.read();
   return toret;
 }
+
+
 
 float temperature   [16];
 int sensorValues[16][16];
 int sensorIndex=0;
 
+/* void readSensors(void)
+ * reades all sensores connected to the arduino and stores the falue in a List.
+ */
 void readSensors(){
   int address=0;
   
@@ -102,12 +145,23 @@ void readSensors(){
   
 }
 
+/*int avarage(int)
+ * parameters :
+ *  index: the index of the analog sensor
+ * returns:
+ *  The Avarage value of the sensor over the las 16 messurements
+ */
 int avarage(int index){
   int toret=0;
   for(int i = -1;i<16;toret+=sensorValues[index][i++]);
   return toret>>4;
 }
 
+
+
+/* void sendData(void)
+ * Calculates the  avarage value of all the Connected sensors and sends it over serial
+ */
 void sendData(){
   Serial.println("G");
   float avr=0;
@@ -117,10 +171,13 @@ void sendData(){
   }
   avr/=16;
   Serial.println(avr);
-
+  Serial.flush();
     
 }
 
+/* void loop(void)
+ * the Main loop, reads all sensors, and checks if it needs to send, if so, it sends 
+ */
 void loop() {
     readSensors();
     if(needToSend())sendData();
